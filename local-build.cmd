2b829e62709e9d25ee81e85d03ec7123efe696ef
dirkarnez <smalldirkalex@gmail.com>
2022-08-22 23:08:14 +0800

    - wip

--- local-build.cmd
+++ local-build.cmd
@@ -15,7 +15,10 @@ D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
 D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;^
 D:\Softwares\cmake-3.23.0-rc1-windows-x86_64\bin;
 
-cmake.exe -G"MinGW Makefiles" -DBOOST_ROOT="D:/Code Projects/cpp-libraries/boost-1.78.0-windows-2019-mingw-static/lib/cmake/Boost-1.78.0" -DCMAKE_BUILD_TYPE=Debug -B./build &&^
+cmake.exe -G"MinGW Makefiles" ^
+-DBOOST_ROOT="D:/Code Projects/cpp-libraries/boost-1.78.0-windows-2019-mingw-static/lib/cmake/Boost-1.78.0" ^
+-DYAMLCPP_ROOT="D:/Code Projects/cpp-libraries/yaml-cpp-v0.7.0-x86_64-posix-seh-rev0-8.1.0" ^
+-DCMAKE_BUILD_TYPE=Debug -B./build &&^
 cd build &&^
 cmake --build . &&^
 echo "Successful build"
--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,26 @@
 #include <iostream>
 #include <vector>
 #include <array>
+#include <map>
 #include <algorithm>
 #include <iomanip>
 #include <boost/regex.hpp>
 
+#include "yaml-cpp/node/node.h"
+#include "yaml-cpp/yaml.h"
+
+#include "yaml-cpp/node/node.h"
+#include "yaml-cpp/emitter.h"
+#include "yaml-cpp/node/convert.h"
+#include "yaml-cpp/node/detail/impl.h"
+#include "yaml-cpp/node/emit.h"
+#include "yaml-cpp/node/impl.h"
+#include "yaml-cpp/node/iterator.h"
+
 using namespace std;
 
-string escape_string_for_regex(string str) {
+string escape_string_for_regex(string str)
+{
    const regex esc("[.^$|()\\[\\]{}*+?\"\\\\]");
    const string rep("\\\\&");
    return regex_replace(str, esc, rep, regex_constants::match_default | regex_constants::format_sed);
@@ -44,9 +57,29 @@ string read_file_content(const string &path)
    return string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
 }
 
+class {
+
+}
 int main()
 {
-   
+   YAML::Node yaml = YAML::LoadFile("config.yaml");
+
+   for (YAML::const_iterator it = yaml.begin(); it != yaml.end(); ++it) {
+      auto fileName = it->first.as<string>();
+      cout << "fileName:" << fileName << endl;
+      auto b = it->second.as<std::vector<std::map<string, string>>>();
+      for (int i = 0; i < b.size(); i++)
+      {
+         
+         cout << "haystack:" << b.at(i)["haystack"] <<endl;
+         cout << "needle:" << b.at(i)["needle"] <<endl;
+         cout << "occurrence:" << b.at(i)["occurrence"] <<endl;
+      }
+   }
+
+
+   cout << "==================";
+
    const vector<array<string, 3>> config = {
        {"project(\"AMA2104-playground\")",
         "AMA2104-playground",
@@ -55,18 +88,6 @@ int main()
         "17",
         "1"}};
 
-   // std::stringstream ss;
-   // for (size_t i = 0; i < config.size() - 1; ++i)
-   // {
-   //    ss << "(" << config[i].at(0) << ")"
-   //       << "|";
-   // }
-   // ss << "(" << config[config.size() - 1].at(0) << ")";
-
-   // string myexpression = ss.str();
-   // cout << myexpression << endl;
-
-   //std::string s = "project(\"hello world\")\ndefproject(\"hello world\") def";
    std::string s = read_file_content("CMakeLists.txt");
 
    for (size_t i = 0; i < config.size() - 1; i++)
@@ -75,28 +96,12 @@ int main()
       cout << current << ":" << substr_occurrence(s, current) << endl;
    }
 
-   // std::match_results<std::string::const_iterator> what;
-   // std::regex_constants::match_flag_type flags = std::regex_constants::match_default;
-
-   // cout << escape_string_for_regex(config.at(0).at(0)) << endl;
-   // std::regex ssss("("+ escape_string_for_regex(config.at(0).at(0)) + ")");
-
-   // std::string::const_iterator searchStart(s.cbegin());
-   // smatch res;
-   // while (regex_search(searchStart, s.cend(), res, ssss))
-   // {
-   //    std::cout << "!" << res[0] << std::endl;
-   //    searchStart = res.suffix().first;
-   // }
-
    std::cout << "++++++++++++++++++++replacing" << std::endl;
-   
 
    boost::regex e2;
    e2.assign("(" + escape_string_for_regex(config.at(0).at(0)) + ")|(def)");
    //"(?1<1>$&</1>)
    auto src = escape_string_for_regex(config.at(0).at(0));
-   
 
    auto new_content = src.replace(src.find(config.at(0).at(1)), config.at(0).at(1).length(), "ypypoypoy");
 
@@ -108,60 +113,3 @@ int main()
    std::cin.get();
    return 0;
 }
-
-// // n placeholder, each must have only one occurance
-// #include <iostream>
-// #include <string>
-// #include <map>
-// #include <boost/regex.hpp>
-
-// purpose:
-// takes the contents of a file in the form of a string
-// and searches for all the C++ class definitions, storing
-// their locations in a map of strings/int's
-// typedef std::map<std::string, int, std::less<std::string> > map_type;
-
-// boost::regex expression(
-//    "^(template[[:space:]]*<[^;:{]+>[[:space:]]*)?"
-//    "(class|struct)[[:space:]]*"
-//    "(\\<\\w+\\>([[:blank:]]*\\([^)]*\\))?"
-//    "[[:space:]]*)*(\\<\\w*\\>)[[:space:]]*"
-//    "(<[^;:{]+>[[:space:]]*)?(\\{|:[^;\\{()]*\\{)");
-
-// void IndexClasses(map_type& m, const std::string& file)
-// {
-//    std::string::const_iterator start, end;
-//    start = file.begin();
-//    end = file.end();
-
-//    boost::match_results<std::string::const_iterator> what;
-//    boost::match_flag_type flags = boost::match_default;
-//    while(regex_search(start, end, what, expression, flags))
-//    {
-//       what[0] contains the whole string
-//       what[5] contains the class name.
-//       what[6] contains the template specialisation if any.
-//       add class name and position to map:
-//       auto q = what[5].first;
-//       auto w =  what[5].second;
-//       auto e = what[6].first;
-//       auto r = what[6].second;
-
-//       m[std::string(q, w) + std::string(e, r)] = q - file.begin();
-//       update search position:
-//       start = what[0].second;
-//       // update flags:
-//       flags |= boost::match_prev_avail;
-//       flags |= boost::match_not_bob;
-//    }
-// }
-
-// int main() {
-//    map_type a;
-
-//    IndexClasses(a, "project(\"hello-world\"");
-//    IndexClasses(a, "class a{}; class b{};");
-
-//    std::cin.get();
-//    return 0;
-// }
